863257151f7bd3a9fb106a78bbba569abbdc3c73
James Harder <james.harder@gmail.com>
2019-06-28 13:05:00 -0500

    Adds work on new style, and changes to goal.js

--- wwwroot/scripts/goals.js
+++ wwwroot/scripts/goals.js
@@ -1,31 +1,50 @@
-function showGoals( JSONGoals ) {
+function renderGoals( JSONGoals ) {
 	// JSONGoal => goalId(int), name(string), isCompleted(bool)
 
 	JSONGoals.forEach(goal => {
-		// create a set of HTML objects for each goal
+		// goalDiv is a 'row' that contains 3 'col's
 		var goalDiv = document.createElement('div');
-		goalDiv.className = 'goal';
+		goalDiv.className = 'row';
 		goalDiv.id = goal.goalId;
 
+		// gcDiv, gdDiv are each one 'col' wide, gnDiv is 10 'col' wide
+		var gcDiv = document.createElement('div');
+		gcDiv.className = 'col s1';
+
+		var gdDiv = document.createElement('div');
+		gdDiv.className = 'col s1';
+
+		var gnDiv = document.createElement('div');
+		gnDiv.className = 'col s10';
+
+		// goalCompleted -> checkbox, goalName -> textfield, goalDelete -> button
 		var goalCompleted = document.createElement('input');
 		goalCompleted.type = 'checkbox';
-		goalCompleted.className = 'goal-completed';
+		goalCompleted.id = 'gc' + goal.goalId;
 		goalCompleted.checked = goal.isCompleted;
+		goalCompleted.setAttribute('style', 'opacity: 100');
 
 		var goalName = document.createElement('input');
 		goalName.type = 'textfield';
-		goalName.className = 'goal-name';
 		goalName.value = goal.name;
+		goalName.setAttribute('style', 'width: inherit');
 
 		var goalDelete = document.createElement('button');
 		goalDelete.type = 'button';
-		goalDelete.className = 'goal-delete';
 		goalDelete.textContent = 'X';
 
-		goalDiv.appendChild(goalCompleted);
-		goalDiv.appendChild(goalName);
-		goalDiv.appendChild(goalDelete);
+		// add elements to respective 'col' divs
+		gcDiv.appendChild(goalCompleted);
+		gnDiv.appendChild(goalName);
+		gdDiv.appendChild(goalDelete);
 
+		// add 'col' divs to 'row' div
+		goalDiv.appendChild(gcDiv);
+		goalDiv.appendChild(gnDiv);
+		goalDiv.appendChild(gdDiv);
+
+		// add 'row' to section
+		var section = document.querySelector('section');
 		section.appendChild(goalDiv)
 
 	});
@@ -47,7 +66,7 @@ function addGoal() {
 	var postRequest = new XMLHttpRequest();
 	
 	// use the request object to open the url using the HTTP GET method
-	postRequest.open( 'POST', requestURL );
+	postRequest.open( 'POST', postRequestURL );
 
 	// set the content-type to JSON
 	postRequest.setRequestHeader('Content-Type', 'application/JSON');
@@ -57,39 +76,45 @@ function addGoal() {
 
 	// send the request
 	postRequest.send(stringGoal);
+
+	showGoals();
 }
 
 function showNewGoalPanel() {
 	
-	$('#new-goal-panel').css( {'display': 'block'} );
+	var newGoalPanel = document.getElementById('new-goal-panel');
+	newGoalPanel.setAttribute('class', 'row');
 
 }
 
-// store a reference to the section element
-var section = document.querySelector('section');
+function showGoals() {	// store a reference to the section element
+	var section = document.querySelector('section');
 
-// store the url that will send us the JSON
-var requestURL = 'api/goal/';
+	// store the url that will send us the JSON
+	var requestURL = 'api/goal/';
 
-// create an XMLHttpRequest object
-var request = new XMLHttpRequest();
+	// create an XMLHttpRequest object
+	var request = new XMLHttpRequest();
 
-// use the request object to open the url using the HTTP GET method
-request.open( 'GET', requestURL );
+	// use the request object to open the url using the HTTP GET method
+	request.open( 'GET', requestURL );
 
-// set the response type
-request.responseType = 'json';
+	// set the response type
+	request.responseType = 'json';
 
-// send the request
-request.send();
+	// send the request
+	request.send();
 
-// when the response is received
-request.onload = function() {
-	// store the response
-	var goals = request.response;
+	// when the response is received
+	request.onload = function() {
+		// store the response
+		var goals = request.response;
 
-	console.log(goals[0])
+		console.log(goals[0])
 
-	// do stuff with it...
-	showGoals( goals );
+		// do stuff with it...
+		renderGoals( goals );
+	}
 }
+
+showGoals();
